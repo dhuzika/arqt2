a87b4e92ce4f964d8fd49439f36572fbf899966f
dhuzika <dhuzika@outlook.com>
2024-11-08 01:21:26 -0300

    feat(t2): implementa solucao vista em aula

---
+++ lembretes/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+app.use(express.json());
+
+const lembretes = {};
+let contador = 0;
+const axios = require('axios')
+
+app.post('/eventos', (req, res) => {
+    try {
+      funcoes[req.body.tipo](req.body.dados);
+    } catch (err) {}
+      res.status(200).send({ msg: 'ok' });
+});
+
+app.get('/lembretes', (req, res) => {
+    res.send(lembretes);
+});
+
+app.put('/lembretes', async (req, res) => {
+    contador++;
+    const { texto } = req.body;
+    lembretes[contador] = { contador, texto };
+
+    await axios.post("http://localhost:10000/eventos", {
+        tipo: "LembreteCriado",
+        dados: { contador, texto }
+    });
+
+    res.status(201).send(lembretes[contador]);
+});
+
+app.listen(4000, () => console.log('Lembretes. Porta 4000'));
